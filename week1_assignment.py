9693d74a48216f830f89bd8ce51ab9bf5bbfb4dc
Mutindadev <rmmutinda02@gmail.com>
2025-08-02 15:01:09 -0700

    added file

---
+++ week1_assignment.py
@@ -0,0 +1,12 @@
+first_number = input("enter the first number")
+second_number = input("enter the second number")
+#sum_of_numbers = int(first_number) + int(second_number)
+#print("The sum of the two numbers is:", sum_of_numbers)
+#print("Thank you for using the sum calculator!")
+
+subtract_number = input("enter the number to subtract from the first number")
+first_number = int(first_number)
+second_number = int(second_number)
+subtract_number = int(subtract_number)
+result = first_number - subtract_number
+print("The result of subtracting", subtract_number, "from", first_number, "is:", result)
