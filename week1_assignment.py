2a51b521ed4b3f01c3f38a3fea3bfdd39177dd47
Mutindadev <rmmutinda02@gmail.com>
2025-08-03 13:58:54 -0700

    added assignment file

--- week1_assignment.py
+++ week1_assignment.py
@@ -10,3 +10,16 @@ second_number = int(second_number)
 subtract_number = int(subtract_number)
 result = first_number - subtract_number
 print("The result of subtracting", subtract_number, "from", first_number, "is:", result)
+number = input("Enter a number to check if it is even or odd: ")
+if number.isdigit():
+    number = int(number)
+    if number % 2 == 0:
+        print(number, "is an even number.")
+    else:
+        print(number, "is an odd number.")
+else:
+    print("Please enter a valid integer.")
+# The code above performs subtraction and checks if a number is even or odd.
+# It prompts the user for two numbers, subtracts a third number from the first,
+# and checks if a given number is even or odd, providing appropriate messages.
+# The code above performs subtraction and checks if a number is even or odd.
